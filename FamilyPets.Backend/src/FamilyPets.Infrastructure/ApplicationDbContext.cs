e8375eee985694c8dc89ee70884ecade850af146
egor.galeznik.87 <egor.galeznik.87@gmail.com>
2025-05-16 21:19:13 +0200

    Connected dabatase 1) Added Docker 2) Added ConectionString into secrets 3) Added EF Core to manage database (only connecting at the moment)

---
+++ FamilyPets.Backend/src/FamilyPets.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using FamilyForPets.Domain.VolunteerAgregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace FamilyForPets.Infrastructure
+{
+    public class ApplicationDbContext(IConfiguration configuration) : DbContext
+    {
+        private const string DATABASE = "Database";
+
+        public DbSet<Pet> Pets => Set<Pet>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString(DATABASE));
+            optionsBuilder.UseSnakeCaseNamingConvention();
+            optionsBuilder.UseLoggerFactory(CreateLoggerFactory());
+        }
+
+        private ILoggerFactory CreateLoggerFactory() =>
+            LoggerFactory.Create(builder => { builder.AddConsole(); });
+    }
+}
