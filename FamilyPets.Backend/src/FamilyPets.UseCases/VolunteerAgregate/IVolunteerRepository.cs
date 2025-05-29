490f3d484a01a0bae9dc9f51a8e8848b61230a41
egor.galeznik.87 <egor.galeznik.87@gmail.com>
2025-05-29 22:22:27 +0200

    Added Volunteer Repository with its interface. Moved DI in separeate file (static class)

---
+++ FamilyPets.Backend/src/FamilyPets.UseCases/VolunteerAgregate/IVolunteerRepository.cs
@@ -0,0 +1,17 @@
+﻿using CSharpFunctionalExtensions;
+using FamilyForPets.Domain.SharedValueObjects;
+using FamilyForPets.Domain.VolunteerAgregate;
+using FamilyForPets.Domain.VolunteerAgregate.VolunteerValueObjects;
+using FamilyForPets.Shared;
+
+namespace FamilyForPets.UseCases.VolunteerAgregate
+{
+    public interface IVolunteerRepository
+    {
+        Task<Result<Guid, Error>> Add(Volunteer volunteer, CancellationToken cancellationToken);
+
+        Task<Result<Volunteer, Error>> GetById(VolunteerId id);
+
+        Task<Result<Volunteer, Error>> GetByEmail(EmailAdress email);
+    }
+}
