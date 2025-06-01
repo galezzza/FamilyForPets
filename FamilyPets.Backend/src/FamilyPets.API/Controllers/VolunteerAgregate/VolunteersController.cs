7493e424540f190125beb762be4a7e6ed3c2a4c5
egor.galeznik.87 <egor.galeznik.87@gmail.com>
2025-06-01 17:36:21 +0200

    Changed Response type to IResult (EndpointResult). Implicited Response from Result<>

--- FamilyPets.Backend/src/FamilyPets.API/Controllers/VolunteerAgregate/VolunteersController.cs
+++ FamilyPets.Backend/src/FamilyPets.API/Controllers/VolunteerAgregate/VolunteersController.cs
@@ -1,13 +1,13 @@
 ﻿using CSharpFunctionalExtensions;
 using FamilyForPets.API.Controllers.VolunteerAgregate.Requests.CreateVolunteer;
 using FamilyForPets.API.ResponsesCommonLogic;
+using FamilyForPets.API.ResponsesCommonLogic.EndpointResults;
 using FamilyForPets.Shared;
 using FamilyForPets.UseCases.Abstractions;
-using FamilyForPets.UseCases.DTOs;
 using FamilyForPets.UseCases.VolunteerAgregate.CreateVolunteer;
 using FluentValidation;
-using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using IResult = Microsoft.AspNetCore.Http.IResult;
 
 namespace FamilyForPets.API.Controllers.VolunteerAgregate
 {
@@ -20,31 +20,25 @@ namespace FamilyForPets.API.Controllers.VolunteerAgregate
     //
     // 2) create Command from Request (they are the same)
     //
-    // 3) validate command using FluentValidation
-    //      if result is not valid return envelope with errors
-    //
     // 4) send to UseCases layer to handle logic
     //      if result is not valid return envelope with errors
     //
     // 5) if ok return envelope with response
+    //      5.1) EndpointResult implicits Result<> to Response
     [ApiController]
     [Route("[controller]")]
     public class VolunteersController : ControllerBase
     {
         [HttpPost]
-        public async Task<ActionResult> Create(
+        public async Task<EndpointResult<Guid>> Create(
             [FromBody] CreateVolunteerRequest request,
             [FromServices] ICommandHandler<CreateVolunteerCommand, Guid> handler,
-            [FromServices] IValidator<CreateVolunteerCommand> validator,
             CancellationToken cancellationToken = default)
         {
             CreateVolunteerCommand command = request.ToCommand();
 
-            Result<Guid, ErrorList> result = await handler.HandleAsync(command, cancellationToken);
-            if (result.IsFailure)
-                return result.Error.ToResponseFromErrorList();
-
-            return Ok(result.ToResponseFromSuccessResult());
+            // implicit from Result<T, E> to ResponseEnvelope
+            return await handler.HandleAsync(command, cancellationToken);
         }
     }
 }
---
+++ FamilyPets.Backend/src/FamilyPets.API/Responses/EndpointResults/EndpointResult.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+using CSharpFunctionalExtensions;
+using FamilyForPets.Shared;
+using IResult = Microsoft.AspNetCore.Http.IResult;
+
+namespace FamilyForPets.API.ResponsesCommonLogic.EndpointResults
+{
+    public class EndpointResult<TValue> : IResult
+    {
+        private readonly IResult _result;
+
+        public EndpointResult(Result<TValue, Error> result)
+        {
+            _result = result.IsSuccess
+                ? new SuccesResult<TValue>(result.Value)
+                : new ErrorsResult(result.Error);
+        }
+
+        public EndpointResult(Result<TValue, ErrorList> result)
+        {
+            _result = result.IsSuccess
+                ? new SuccesResult<TValue>(result.Value)
+                : new ErrorsResult(result.Error);
+        }
+
+        public Task ExecuteAsync(HttpContext httpContext) =>
+            _result.ExecuteAsync(httpContext);
+
+        public static implicit operator EndpointResult<TValue>(Result<TValue, Error> result)
+            => new EndpointResult<TValue>(result);
+
+        public static implicit operator EndpointResult<TValue>(Result<TValue, ErrorList> result)
+            => new EndpointResult<TValue>(result);
+    }
+}
