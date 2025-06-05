a57556a92ba6dd1e4daacf730811396eb33dde38
egor.galeznik.87 <egor.galeznik.87@gmail.com>
2025-06-05 16:37:43 +0200

    Connected Volunteer.API layer into main API layer

---
+++ FamilyPets.Backend/src/Volunteer/FamilyForPets.Volunteer.API/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyForPets.Framework
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("{id:guid}/sdadas")]
+        public async Task<Guid> SomeMethodName(
+            [FromRoute] Guid id,
+            CancellationToken cancellationToken = default)
+        {
+            return id;
+        }
+    }
+}
