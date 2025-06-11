6fdea37a511e4ac116feee05872d52f2c887da00
egor.galeznik.87 <egor.galeznik.87@gmail.com>
2025-06-11 18:27:47 +0200

    Added soft deletion for Volunteer and Pet as domain logic

---
+++ FamilyPets.Backend/src/Shared/FamilyForPets.SharedKernel/SoftDeletableEntity.cs
@@ -0,0 +1,29 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace FamilyForPets.SharedKernel
+{
+    public abstract class SoftDeletableEntity<TId> : Entity<TId>
+        where TId : IComparable<TId>
+    {
+        protected SoftDeletableEntity(TId id)
+            : base(id)
+        {
+        }
+
+        public bool IsDeleted { get; private set; }
+
+        public DateTime? DeletionDate { get; private set; }
+
+        public virtual void SoftDelete()
+        {
+            IsDeleted = true;
+            DeletionDate = DateTime.Now;
+        }
+
+        public virtual void Restore()
+        {
+            IsDeleted = false;
+            DeletionDate = null;
+        }
+    }
+}
